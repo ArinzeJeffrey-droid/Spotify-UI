9445c0aa7eec6a818c600acdfe62b376dea67ec0
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-05-20 22:37:19 +0100

    Create grid layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import PlayListCard from "@/components/PlayListCard";
+import { Card } from "@/components/ui/card";
+import "./styles/index.css";
 
 export default function Home() {
   return (
-    <main className="">
-      <PlayListCard />
+    <main className="main">
+      <section className="py-2 px-1">
+        <Card className="h-10 bg-spotify-dark border-none"></Card>
+      </section>
+      <section className="py-2 px-1">
+        <Card className="h-10 bg-spotify-dark border-none"></Card>
+      </section>
+      <section className="py-2 px-1">
+        <Card className="h-10 bg-spotify-dark border-none"></Card>
+      </section>
     </main>
   );
 }
