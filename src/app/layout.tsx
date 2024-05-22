40b6526b1373c261bd8504bfda0d0a7ed907564e
Arinze Jeffrey <61787049+ArinzeJeffrey-droid@users.noreply.github.com>
2024-05-22 17:03:39 +0100

    [#pqjwpb63] Re-organize components layout (#11)
    * [#pqjwpb63] Re-organize components layout
    
    Before I add more contents, I need to reorganize the layout/components
    structure to better prepare for the the routing implementation.
    
    E.g
    
    Making sure the `Sidebar` component in the first column layout is always
    on the page when navigating.
    
    https://trello.com/c/pqjwpb63
    
    * Move Sidebar to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import clsx from "clsx";
+import { Card } from "@/components/ui/card";
+import Playback from "@/components/Playback/Playback";
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import "./styles/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, "bg-black")}>{children}</body>
+      <body className={clsx(inter.className, "bg-black overflow-hidden")}>
+        <main className="flex h-screen gap-2">
+          <Sidebar />
+          <div className="page-wrapper">
+            <section className="pt-2">
+              <Card className="h-10 bg-spotify-dark border-none">
+                {children}
+              </Card>
+            </section>
+            <section className="pt-2">
+              <Card className="h-10 bg-spotify-dark border-none"></Card>
+            </section>
+          </div>
+        </main>
+        <Playback className="w-full fixed bottom-0 left-0" />
+      </body>
     </html>
   );
 }
