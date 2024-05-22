abcc2ae1bb8d42c942be587789c55cfa803ec2b3
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-05-22 17:02:24 +0100

    Move Sidebar to layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import { Card } from "@/components/ui/card";
-import "./styles/index.css";
-import Playback from "@/components/Playback/Playback";
-import Sidebar from "@/components/Sidebar";
-
 export default function Home() {
   return (
-    <main className="flex h-screen gap-2">
-      <Sidebar />
-      <div className="page-wrapper">
-        <section className="pt-2">
-          <Card className="h-10 bg-spotify-dark border-none"></Card>
-        </section>
-        <section className="pt-2">
-          <Card className="h-10 bg-spotify-dark border-none"></Card>
-        </section>
-      </div>
-
-      <Playback className="w-full fixed bottom-0 left-0" />
-    </main>
+    <div>
+      <h1>Home Page</h1>
+    </div>
   );
 }
