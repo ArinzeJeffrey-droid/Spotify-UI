eeb35f7bcf74a06b7e477b17eb4a0ed9c31f7e03
Arinze Jeffrey <61787049+ArinzeJeffrey-droid@users.noreply.github.com>
2024-05-26 18:49:12 +0100

    [#bg4XklpR] Add tooltip for Playback Icons (#14)
    * [#bg4XklpR] Add tooltip for Playback Icons
    
    I should add a tooltip for the icons in the Playback control to help
    describe what each controls do when a user hovers on them.
    
    ![image.png](https://trello.com/1/cards/664c96be748ba9a6718797e4/attachments/664c9719a0b24ad777cf21a2/download/image.png)
    
    https://trello.com/c/bg4XklpR
    
    * Add tooltip for Playback Icons.

--- src/components/Playback/PlaybackControls.tsx
+++ src/components/Playback/PlaybackControls.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Shuffle,
@@ -6,16 +7,43 @@ import {
   PauseCircle,
   Repeat,
 } from "react-feather";
+import { Tooltip } from "react-tooltip";
+import Button from "../Button";
+
+const tooltips = [
+  { id: "shuffle", tooltip: "Shuffle" },
+  { id: "previous", tooltip: "Previous" },
+  { id: "pause", tooltip: "Pause" },
+  { id: "next", tooltip: "Next" },
+  { id: "repeat", tooltip: "Repeat" },
+];
 
 const PlaybackControls = () => {
   return (
-    <div className="flex items-center justify-center gap-5">
-      <Shuffle className="text-spotify-playback" />
-      <SkipBack fill="#BCBABB" stroke="#BCBABB" />
-      <PauseCircle size={50} fill="#BCBABB" />
-      <SkipForward fill="#BCBABB" stroke="#BCBABB" />
-      <Repeat className="text-spotify-playback" />
-    </div>
+    <React.Fragment>
+      <div className="flex items-center justify-center gap-5">
+        <Button id="shuffle">
+          <Shuffle className="text-spotify-playback" />
+        </Button>
+        <Button id="previous">
+          <SkipBack fill="#BCBABB" stroke="#BCBABB" />
+        </Button>
+        <Button id="pause">
+          <PauseCircle size={50} fill="#BCBABB" />
+        </Button>
+        <Button id="next">
+          <SkipForward fill="#BCBABB" stroke="#BCBABB" />
+        </Button>
+        <Button id="repeat">
+          <Repeat className="text-spotify-playback" />
+        </Button>
+      </div>
+      {tooltips.map(({ id, tooltip }) => (
+        <Tooltip key={id} anchorSelect={`#${id}`} place="top">
+          {tooltip}
+        </Tooltip>
+      ))}
+    </React.Fragment>
   );
 };
 
